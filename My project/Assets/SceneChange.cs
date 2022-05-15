1e2475da757d4146871c8fe474f9532e238d2c77
Xin Xie <D17128351@mydit.ie>
2022-05-15 08:36:32 +0800

    Assignment

---
+++ My project/Assets/SceneChange.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public float countTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountTime();
+        ToBreak();
+    }
+
+    void CountTime()
+    {
+        countTime += Time.deltaTime;
+        if(countTime > 30.0f)
+        {
+            SceneManager.LoadScene("Battle");
+        }
+    }
+
+    void ToBreak()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            countTime = 0f;
+        }
+    }
+}
